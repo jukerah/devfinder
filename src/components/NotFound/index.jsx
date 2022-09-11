f6ac5d20f5d5c7073dd49c92c2c29ab83ba90336
Mario Elvio <81795443+jukerah@users.noreply.github.com>
2022-09-11 03:09:30 -0300

    Delete src directory

--- src/components/NotFound/index.jsx
+++
@@ -1,3 +0,0 @@
-export const NotFound = () => {
-  return <p className='text-kashmirBlue dark:text-white flex items-center bg-white dark:bg-cloudBurst p-6 rounded-2xl drop-shadow-md'>No data found.</p>;
-}
